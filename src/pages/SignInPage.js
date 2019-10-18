37d52577c835d9f5c8132c3322ba993d74acb864
Wipawadee Monkhut <wipawadee.mo@ku.th>
2019-10-18 15:37:49 +0700

    add SignInPage

---
+++ src/pages/SignInPage.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import '../App.css';
+
+export default class SignInPage extends Component {
+  render() {
+    return (
+      <div className="sign-in-page">
+        <div></div>
+        <div className="center-sign-in-page">
+          <div className="sign-in-canvas">
+            <div className="left-sign-in-canvas">
+              1
+            </div>
+            <div className="right-sign-in-canvas">
+              2
+            </div>
+          </div>
+        </div>
+        <div></div>
+      </div>
+    )
+  }
+}
