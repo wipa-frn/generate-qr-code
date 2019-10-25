1b193d235f3130a2ef8f3ff6ada43252d5f70359
Wipawadee Monkhut <wipawadee.mo@ku.th>
2019-10-25 16:42:49 +0700

    add Route path

--- src/App.js
+++ src/App.js
@@ -4,13 +4,26 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import SignInPage from './pages/SignInPage'
 import MainPage from './pages/MainPage'
 import GenerateQRCodePage from './pages/GenerateQRCodePage'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useParams
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      {/* <SignInPage /> */}
-      <GenerateQRCodePage />
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path="/" ><SignInPage /></Route>
+          <Route path="/sign-in" ><SignInPage /></Route>
+          <Route path="/generate-qr-code" ><GenerateQRCodePage /></Route>
+        </Switch>
+      </div>
+    </Router>
+
   );
 }
 
